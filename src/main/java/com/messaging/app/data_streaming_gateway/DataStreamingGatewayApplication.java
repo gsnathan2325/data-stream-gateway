7a0779700e599f1c2af415ce4ba3372498f4bf03
gsnathan2325 <gsnathan77@yahoo.com>
2025-09-02 22:25:17 -0400

    test

---
+++ src/main/java/com/messaging/app/data_streaming_gateway/DataStreamingGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.messaging.app.data_streaming_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStreamingGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStreamingGatewayApplication.class, args);
+		System.out.println("TEST");
+	}
+
+}
---
+++ src/test/java/com/messaging/app/data_streaming_gateway/DataStreamingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messaging.app.data_streaming_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStreamingGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
