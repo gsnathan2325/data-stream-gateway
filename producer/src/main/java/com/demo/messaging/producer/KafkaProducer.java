bb9534d613bfcbad09ae5f0b969c8eb72c8d069e
gsnathan2325 <gsnathan77@yahoo.com>
2025-09-04 07:50:35 -0400

    producer added

---
+++ producer/src/main/java/com/demo/messaging/producer/KafkaProducer.java
@@ -0,0 +1,26 @@
+package com.demo.messaging.producer;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam String messsage) {
+        kafkaTemplate.send("my-topic", messsage);
+
+        return "Message Send " +messsage;
+    }
+
+}
