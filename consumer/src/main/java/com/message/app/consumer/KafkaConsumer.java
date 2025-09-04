fb2401b8f7e4ec14db8b8729028214a3ec140d0d
gsnathan2325 <gsnathan77@yahoo.com>
2025-09-04 17:41:39 -0400

    consumer  object message processing

--- consumer/src/main/java/com/message/app/consumer/KafkaConsumer.java
+++ consumer/src/main/java/com/message/app/consumer/KafkaConsumer.java
@@ -3,13 +3,22 @@ package com.message.app.consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.message.app.consumer.model.Employee;
+
 @Component
 public class KafkaConsumer {
 
-    @KafkaListener(topics  = "my-topic" , groupId = "my-group")
-    public void listem(String message){
+    @KafkaListener(topics = "my-topic", groupId = "my-group")
+    public void listen(String message) {
 
-    System.out.println("Received Message..."+message);
+        System.out.println("Received Message..." + message);
+
+    }
+
+    @KafkaListener(topics = "my-topic-new", groupId = "my-group")
+    public void listen2(Employee employee) {
+
+        System.out.println("Received Message from Data Consumer " + employee.getId());
 
     }
 
---
+++ consumer/src/main/java/com/message/app/consumer/model/Employee.java
@@ -0,0 +1,45 @@
+package com.message.app.consumer.model;
+
+public class Employee {
+
+    private String id;
+    private String first_name;
+    private String last_name;
+
+    
+    
+    public Employee() {
+    }
+
+    public Employee(String id, String first_name, String last_name) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+    
+
+}
